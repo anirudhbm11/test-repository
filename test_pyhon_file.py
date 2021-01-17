97e1ecd283f141177cb151daf8851589d1f9e3d8
Anirudh B M <bmanirudh69@gmail.com>
2021-01-18 00:27:26 +0530

    Corrected div func

--- test_pyhon_file.py
+++ test_pyhon_file.py
@@ -5,7 +5,11 @@ def subtract(x,y):
     return x-y
 
 def divide(x,y):
-    return x/y
+    if y == 0:
+        print("Cannot divide")
+        return
+    else:
+        return x/y
 
 if __name__ == "__main__":
     ans = multiply(2,3)
