67605084a4d9446540e77d2a2b7e48491557ed96
Anirudh B M <bmanirudh69@gmail.com>
2021-01-18 01:06:03 +0530

    One more change with merge with changingDiv with corrrect code

--- test_pyhon_file.py
+++ test_pyhon_file.py
@@ -5,16 +5,12 @@ def subtract(x,y):
     return x-y
 
 def divide(x,y):
-<<<<<<< HEAD
-    pass
-=======
     if y == 0:
         print("Cannot divide")
         print("\n use another number")
         return
     else:
         return x/y
->>>>>>> changingDiv
 
 if __name__ == "__main__":
     ans = multiply(2,3)
